34ce4c5493e4515e45887b859a500d4d67c19ddf
carisaelam <carisaelam@gmail.com>
2024-09-13 17:48:49 -0400

    Make limit and search term dynamic artist page
    Pulled out the limit and the search terms from
    the url on the gallery page so they can be
    dynamically changed. I want to eventually include
    a search bar on this page as well.

--- artist.js
+++ artist.js
@@ -4,12 +4,14 @@ const artContainer = document.querySelector('.art__container');
 const artistTitle = document.querySelector('.artist__gallery__title');
 
 let artList = [];
+let searchTerm = 'Claude Monet';
+let limit = 3;
 const IMAGE_CONFIG = 'https://www.artic.edu/iiif/2';
 
 async function getArtByArtist() {
   try {
     const response = await fetch(
-      'https://api.artic.edu/api/v1/artworks/search?q=claude_monet&fields=artist_title,image_id,id,title&limit=100'
+      `https://api.artic.edu/api/v1/artworks/search?q=${searchTerm}&fields=artist_title,image_id,id,title&limit=100`
     );
     const data = await response.json();
     const { iiif_url: config } = data.config;
@@ -23,19 +25,21 @@ async function getArtByArtist() {
 
 async function handleArtByArtist() {
   const art = await getArtByArtist();
-  const tenArts = art.slice(0, 10);
-  buildArtInfoObject(tenArts);
+  const arts = art.slice(0, limit);
+  buildArtInfoObject(arts);
 }
 
 function buildArtInfoObject(art) {
   art.forEach((art) => {
-    const artInfo = {
-      artist: art.artist_title,
-      title: art.title,
-      imageId: art.image_id,
-      id: art.id,
-    };
-    artList.push(artInfo);
+    if (art.artist_title && art.image_id) {
+      const artInfo = {
+        artist: art.artist_title,
+        title: art.title,
+        imageId: art.image_id,
+        id: art.id,
+      };
+      artList.push(artInfo);
+    }
   });
   displayArt(artList);
 }
@@ -50,9 +54,13 @@ function displayArt(list) {
     itemDiv.classList.add('artwork__container');
     itemDiv.innerHTML = `
       <div class="item__image__container"> 
-        <img src="${IMAGE_CONFIG}/${item.imageId}/full/843,/0/default.jpg" alt="${item.title}" class="item__image" >
+        
+        <a href="https://www.artic.edu/artworks/${item.id}/" target="_blank">
+          <img src="${IMAGE_CONFIG}/${item.imageId}/full/843,/0/default.jpg" alt="${item.title}" class="item__image" >
+        </a>
       </div>
       <h3 class="item__title">${item.title}</h3>      
+      <h3 class="item__artist">${item.artist}</h3>      
 
     `;
     artContainer.appendChild(itemDiv);
