270e001c66d1eee201adb03c52f884627a1f91cd
carisaelam <carisaelam@gmail.com>
2024-09-13 13:59:34 -0400

    Update background with primary image color
    Pulls primary image color hsl values from API
    and updates the background color of the main
    container as that color.

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const ENDPOINT = 'https://api.artic.edu/api/v1/artworks';
 const IMAGE_REQUEST_PARAMS = 'full/843,/0/default.jpg';
 
 const id = 26314;
-const fields = '?fields=title,id,image_id,artist_title,color';
-const url = `${ENDPOINT}/${id}/${fields}`;
+let keyword;
+const fields = 'fields=title,id,image_id,artist_title,color';
+let url = `${ENDPOINT}/${id}/?${fields}`;
 
 // Retrieves all art from API. Useful as a helper.
 async function getAllArt() {
@@ -14,6 +15,8 @@ async function getAllArt() {
 
 // Builds object representing a single image
 async function buildImageInfo() {
+  console.log(`running buildImageInfo with url: ${url}`);
+
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -39,6 +42,7 @@ async function buildImageInfo() {
 
 // Calls buildImageInfo and inserts fetched image into artContainer div
 function displayArt() {
+  console.log(`running displayArt with url: ${url}`);
   buildImageInfo()
     .then((imageInfo) => {
       if (imageInfo) {
@@ -57,4 +61,82 @@ function displayArt() {
     .catch((error) => {
       console.error(error);
     });
+}
+
+// SEARCH FORM
+
+const searchForm = document.querySelector('.search__form');
+
+function getKeyword(e) {
+  e.preventDefault();
+  const input = document.getElementById('keyword__input');
+  const newKeyword = input.value;
+
+  buildKeywordSearchUrl(newKeyword);
+}
+
+searchForm.addEventListener('submit', getKeyword);
+
+function buildKeywordSearchUrl(keyword) {
+  console.log(`buildKeywordSearchUrl by: ${keyword}`);
+  searchUrl = `${ENDPOINT}/search?q=${keyword}&${fields}`;
+  console.log(`searchUrl: ${searchUrl}`);
+  buildKeywordSearchImageInfo(searchUrl);
+}
+
+const searchedImages = [];
+
+async function buildKeywordSearchImageInfo(searchUrl) {
+  console.log(`buildKeywordSearchImageInfo by: ${searchUrl}`);
+
+  const response = await fetch(searchUrl);
+  if (!response.ok) {
+    throw new Error(`${response.status}`);
+  }
+  const data = await response.json();
+  const config = data.config.iiif_url;
+
+  const images = data.data;
+
+  images.forEach((image) => {
+    const imageId = image.image_id;
+    const title = image.title;
+    const artist = image.artist_title;
+    const color = image.color;
+
+    const imageUrl = `${config}/${imageId}/${IMAGE_REQUEST_PARAMS}`;
+    const imageInfo = {
+      url: imageUrl,
+      title: title,
+      artist: artist,
+      color: color,
+    };
+    searchedImages.push(imageInfo);
+  });
+  console.log('searchedImages array: ', searchedImages);
+  getTopImage(searchedImages);
+}
+
+function getTopImage(array) {
+  console.log('get top image running');
+  console.log(array);
+  displaySingleImage(array[0]);
+}
+
+function displaySingleImage(image) {
+  if (image) {
+    const artContainer = document.querySelector('.art__container');
+    const mainContainer = document.querySelector('.main__container');
+
+    artContainer.innerHTML = `
+            <img src='${image.url}'>
+            <h3>${image.title}</h3>
+            <p>${image.artist}</p>
+            <p>hsl(${image.color.h}, ${image.color.s}%, ${image.color.l}%)</p>
+
+            `;
+    mainContainer.style.backgroundColor = `hsl(${image.color.h}, ${image.color.s}%, ${image.color.l}%)`;
+  } else {
+    console.error(`Invalid image URL.`);
+  }
 }
