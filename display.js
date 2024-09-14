a42b169f1c2367d872120ec2d81e18a29786ca72
carisaelam <carisaelam@gmail.com>
2024-09-14 10:52:44 -0400

    Add check for null description value
    If image description is null from API, display a
    link for "Learn More" that takes user to the
    Art Institute of Chicago page for that piece of
    art.

--- display.js
+++ display.js
@@ -99,6 +99,22 @@ function displaySingleImage(image) {
     const artContainer = document.querySelector('.keyword__art__container');
     const body = document.querySelector('.body');
 
+    let descriptionHTML = `<p>No description available. </p><a href="https://www.artic.edu/artworks/${image.id}/" target="_blank">
+    <p>Learn More Here!</p>    
+  </a>;`;
+    if (image.description) {
+      descriptionHTML = ` <h3>Description</h3>
+      <p>${image.description}</p>`;
+    }
+
+    let subjectTitleHTML = '';
+    if (image.subjectTitle) {
+      subjectTitleHTML = `<h3>Subject Title</h3>
+      <p>${image.subjectTitle}</p>`;
+    }
+
+ 
+
     artContainer.innerHTML = `
       <img src="${image.url}" alt="${image.title}">
       
@@ -109,9 +125,10 @@ function displaySingleImage(image) {
          <p>${image.artist}</p>    
        </a>
 
-       <a href="https://www.artic.edu/artworks/${image.id}/" target="_blank">
-         <p>Learn More</p>    
-       </a>
+       ${descriptionHTML}
+       ${subjectTitleHTML}
+
+       
       
     `;
     body.style.backgroundColor = `hsl(${image.color.h}, ${image.color.s}%, ${image.color.l}%)`;
